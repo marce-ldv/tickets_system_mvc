2b18085e51b88ad7fd52ee6509ac257c54a61d6e
IgnacioChiaradia <39503621+IgnacioChiaradia@users.noreply.github.com>
2018-10-22 01:19:52 -0300

    agregue la clase artists

---
+++ src/Model/Artist.php
@@ -0,0 +1,19 @@
+<?php
+namespace Model;
+
+class Artist
+{
+	private $id_artist;
+	private $name;
+	private $surname;
+	private $nick_name;
+
+    public function __construct($id_artist)
+    {
+        
+    }
+}
+
+
+
+ ?>
