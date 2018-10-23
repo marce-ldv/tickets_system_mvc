2a839f54e8a2abfb8d565dbef665baaafb67a335
IgnacioChiaradia <39503621+IgnacioChiaradia@users.noreply.github.com>
2018-10-23 16:53:50 -0300

    Se modifico la bbdd del artista y se creo singleton

---
+++ src/Dao/ArtistBDDAO.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dao;
+
+/**
+ * summary
+ */
+class ArtistBDDAO extends SingletonDAO
+{
+	private $list = array();
+	private $table = "artists";
+
+	public function save($object)
+	{
+		$sql = "INSERT INTO $this->table (name) value (:name)";
+
+		$connection = Connection::connect();
+
+		$sentence = $connection->prepare($sql);
+
+		$name = $object->getName();
+		
+		$sentene->bindParam(":name" , $name);
+	}
+
+    
+}
+
---
+++ src/Dao/SingletonDAO.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Dao;
+
+class SingletonDAO
+{
+	private static $instance = array();
+
+	static function getInstance()
+	{
+		$class = get_called_class();
+
+		if(!isset(self::$instance[$class]))
+		{
+			self::$instance[$class] = new $class;
+		}
+
+		return self::$instance[$class];
+	}
+}
