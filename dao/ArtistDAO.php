f643165ce20607d4e4ef9c307967f5a5fb74b9c7
IgnacioChiaradia <39503621+IgnacioChiaradia@users.noreply.github.com>
2018-10-26 20:40:20 -0300

    Cambios en ArtistDao: se agrego el update, delete y se modifico el read

--- dao/ArtistDAO.php
+++ dao/ArtistDAO.php
@@ -3,16 +3,18 @@
 namespace dao;
 
 use model\Artist as Artist;
+use Interface\ICrud as ICrud;
 
-class ArtistDAO extends SingletonDAO
+class ArtistDAO extends SingletonDAO implements ICrud
 {
 	private $table = "artists";
 	private $list = array();
 	private static $instance;
+	private $pdo;
 
-
-	public function __construct(){
-
+	public function __construct()
+	{
+		$this->pdo = new Connection();
 	}
 
 	// no lo necesito porque esta el singletonDAO
@@ -26,14 +28,13 @@ class ArtistDAO extends SingletonDAO
 	}*/
 
 
-	public function addArtist(Artist $artist)
+	public function create($value)
 	{
 		try
 		{
 			$sql = "INSERT INTO $this->table (name) VALUES (:name)";
 
-			$pdo = new Connection();
-			$connection = Connection::connect(); // probar si funciona
+			$connection = $this->pdo->connect(); // probar si funciona $connection = Connection::connect();
 			$statement = $connection->prepare($sql);
 
 			$name = $object->getNameArtist();
@@ -56,13 +57,55 @@ class ArtistDAO extends SingletonDAO
 		}
 	}
 
-    public function fetchAllArtist()
+	/*TODO: PROBAR READ Y READALL EN LA CONTROLADORA*/
+
+	public function read($id)
+	{
+		try {
+
+			$sql = "SELECT * FROM $this->table WHERE artists_id = $id";
+
+			$pdo = new Connection(); // <- en vez de esta y
+			$connection = $pdo->connect(); // esta linea se puede poner $connection = Connection::connect();
+			$statement = $connection->prepare($sql);
+
+			$statement->execute();
+
+			$dataSet[] = $statement->fetch(\PDO::FETCH_ASSOC);
+
+			if($dataSet[0])  // como siempre va a traer un solo objeto pongo dataSet[0] ya que estoy 
+			{
+				$this->mapMethod($dataSet);
+			}
+
+			if(!empty($this->list[0]))
+			{
+				return $this->list[0];
+			}
+
+			return false;
+			
+		}
+		catch (\PDOException $e) 
+		{
+			echo $e->getMessage();
+			die();			
+		}
+		catch (Exception $e)
+		{
+			echo $e->getMessage();
+			die();
+		}
+
+	}
+
+
+    public function readAll()
     {
     	try
     	{
     		$sql = "SELECT * FROM $this->table";
 
-    		$pdo = new Connection();
     		$connection = Connection::connect();
     		$statement = $connection->prepare($sql);
 
@@ -70,7 +113,7 @@ class ArtistDAO extends SingletonDAO
 
 			$dataSet = $statement->fetchAll(\PDO::FETCH_ASSOC);
 
-			$this->mapMethod($dataSet); // realizar metodo
+			$this->mapMethod($dataSet); 
 
 			if(!empty($this->objInstances))
 			{
@@ -90,4 +133,78 @@ class ArtistDAO extends SingletonDAO
     		die();
     	}
     }
+
+    public function update($value)
+    {
+    	try
+    	{
+    		$sql = "UPDATE this->table SET name = :name WHERE id_artist = :id ";
+
+			$connection = Connection::connect();
+    		$statement = $connection->prepare($sql);
+
+    		$name = $value->getNameArtist();
+    		$id = $value->getIdArtist();
+
+    		$statement->bindParam(":name",$name);
+    		$statement->binParam(":id",$id);
+
+    		$statement->execute();
+    	}
+    	catch(\PDOException $e)
+    	{
+    		echo $e->getMessage();
+    		die();
+    	}
+    	catch(Exception $e)
+    	{
+    		echo $e->getMessage();
+    		die();
+    	}
+	}
+
+	public function delete($id)
+	{
+		try
+		{
+			$sql = "DELETE FROM $this->table WHERE id_artist = $id "; // si es un string poner \" $id \";
+
+			$connection = Connection::connect();
+			$statement = $connection->prepare($sql);
+
+
+
+			$statement->execute;
+		}
+	}
+
+	public function mapMethod($dataSet)
+	{
+		$dataSet = is_array($dataSet) ? $dataSet : false;
+
+		if($dataSet)
+		{
+			$this->listado = array_map(function ($p)
+			{
+				$u = new Usuario($p['name']);
+				$u->setIdArtist($p['id_artist']);
+
+				return $u;
+			
+			}, $dataSet); // cierre del array_map
+		}
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
